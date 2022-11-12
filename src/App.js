e3a688122dd0ef6ff639f6893d0204c7dfc84bb4
kaushlendrashukla <kaushlendrashukla2015@gmail.com>
2022-11-12 19:38:52 +0530

    task-9

--- src/App.js
+++ src/App.js
@@ -3,12 +3,46 @@
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet paper',
+      amount: 12,
+      location: 'delhi',
+      date: new Date(2022, 11, 11),
+    },
+    {
+      id: 'e2',
+      title: 'movie',
+      amount: 120,
+      location: 'delhi',
+      date: new Date(2022, 11, 11),
+    },
+    {
+      id: 'e3',
+      title: 'food',
+      amount: 312,
+      location: 'delhi',
+      date: new Date(2022, 11, 11),
+    },
+    {
+      id: 'e4',
+      title: 'fun',
+      amount: 412,
+      location: 'delhi',
+      date: new Date(2022, 11, 11),
+    },
+  ];
   return (
-    <div >
+    <div>
       <h2>Lets get started</h2>
-      <ExpenseItem></ExpenseItem>
-      </div>
+
+      {expenses.map((el, index) => <ExpenseItem title={expenses[index].title} amount={expenses[index].amount} location={expenses[index].location} date={expenses[index].date}></ExpenseItem>)}
+
+    </div>
   );
 }
+
+
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,26 @@
-function ExpenseItem() {
-    return (
-    <div>
-       <ul> Expense Item
-       <li>Food Rs 10</li>
-       <li>Petrol Rs 100</li>
-      <li>Movies Rs 200</li> 
-       </ul>
-     </div>
-        );
+import "./ExpenseItem.css";
+
+function ExpenseItem(props) {
+   // const expensedate = new Date(2022, 11, 11);
+   // const expensetitle = 'car insurance';
+   // const expenseamount = 455;
+   //const LocationOfExpenditure = "cinema"
+
+   return (
+      <div className="expense-item">
+         <div>
+            <div>{props.date.toISOString()}</div>
+
+         </div>
+         <div className="location">{props.location}</div>
+
+         <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">{props.amount}</div>
+         </div>
+
+      </div>
+   );
 }
 
 export default ExpenseItem;
