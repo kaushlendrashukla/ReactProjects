f379b1d68cab0c91402add7845ee238c7293d239
kaushlendrashukla <kaushlendrashukla2015@gmail.com>
2022-11-16 12:48:02 +0530

    advance task3

--- src/App.js
+++ src/App.js
@@ -33,11 +33,19 @@ const App = () => {
       date: new Date(2022, 11, 11),
     },
   ];
+  // {expenses.map((el, index) => <ExpenseItem title={expenses[index].title} amount={expenses[index].amount} location={expenses[index].location} date={expenses[index].date}></ExpenseItem>)}
+
   return (
     <div>
       <h2>Expense Tracker</h2>
-
+      {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} location={expenses[0].location} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} location={expenses[1].location} date={expenses[0].date}></ExpenseItem>
+      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} location={expenses[2].location} date={expenses[2].date}></ExpenseItem>
+      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} location={expenses[3].location} date={expenses[3].date}></ExpenseItem> */}
       {expenses.map((el, index) => <ExpenseItem title={expenses[index].title} amount={expenses[index].amount} location={expenses[index].location} date={expenses[index].date}></ExpenseItem>)}
+
+
+
 
     </div>
   );
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,21 +1,34 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css";
 import ExpenseDetails from "./ExpenseDetails.js";
+import ExpenseAmount from "./UI/ExpenseAmount.js";
 //import Card from "./UI/Card.js";
 
 const ExpenseItem = (props) => {
-const clickhandler =() => {
-  console.log('delete data')
-}
+   let [title,setTitle]  = useState(props.title);
+   let [amount,setAmount] = useState(props.amount)
+   //let [amount,setAmount] = useState(props.amount);
+   
+   let clickhandler = () => {
+      setTitle('updated'); 
+      console.log(title)
+   }
+   let amountHandler = () => {
+      setAmount('$100');
+      console.log('amountClicked')
+   }
    return (
       <div className="expense-item">
          <ExpenseDate date={props.date} />
          <div className="location">{props.location}</div>
          <div className="expense-item__description">
-            <h2>  <ExpenseDetails title={props.title} /> </h2>
-            <div className="expense-item__price">{props.amount}</div>
+            {/* <h2>  <ExpenseDetails title={props.title} /> </h2> */}
+            <h2>{title}</h2>
+            <div className="expense-item__price">{amount}</div>
          </div>
-         <button onClick={clickhandler}>delete</button>
+         <button onClick={amountHandler}>Expense</button>
+         <button onClick={clickhandler}>Title</button>
 
       </div>
    );
